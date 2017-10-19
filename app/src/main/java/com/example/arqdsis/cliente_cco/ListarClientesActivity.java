6e640666e5ca3a979d2c79703b79c6e2bb28852f
monicagomes <margarida>
2017-10-19 19:56:55 -0200

    Alterado o LayoutMain

---
+++ app/src/main/java/com/example/arqdsis/cliente_cco/ListarClientesActivity.java
@@ -0,0 +1,13 @@
+package com.example.arqdsis.cliente_cco;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ListarClientesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_clientes);
+    }
+}
