ef5225ac2dc194c004fea97e47a8ed7ae04b36c5
monicagomes <margarida>
2017-10-19 20:32:06 -0200

    Alter 3

--- app/src/main/java/com/example/arqdsis/cliente_cco/ListarClientesActivity.java
+++ app/src/main/java/com/example/arqdsis/cliente_cco/ListarClientesActivity.java
@@ -1,13 +1,25 @@
 package com.example.arqdsis.cliente_cco;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class ListarClientesActivity extends Activity {
-
+    ArrayList<String> clientes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar_clientes);
+        Intent intent = getIntent();
+        String chave = (String)intent.getStringExtra(MainActivity.CHAVE);
+        clientes = Data.buscaClientes(chave);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, clientes);
+
     }
+
+
 }
--- app/src/main/java/com/example/arqdsis/cliente_cco/MainActivity.java
+++ app/src/main/java/com/example/arqdsis/cliente_cco/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.arqdsis.cliente_cco;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText text;
+    public static final String CHAVE = "com.example.arqdsis.cliente_cco.chave";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (EditText)findViewById(R.id.buscar_clientes); //tranformar em um objeto java
+    }
+
+    public void buscarCliente(View view){
+        Intent intent = new Intent(this, ListarClientesActivity.class);
+        String texto = text.getText().toString();
+        intent.putExtra(CHAVE, texto);0
+        startActivity(intent);
     }
 }
